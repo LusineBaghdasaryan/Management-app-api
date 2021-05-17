f2630f950e6c850a13852cf64578951908d876a9
lusine baghdasaryan <lusinebaghdasaryan1994@gmail.com>
2021-05-17 17:57:14 +0400

    commit

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ app.use(cors());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With,Content-Type,Accept'
-    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
     next();
 });
+;
 
 require('./database/index')(configs, mongoose);
 require('./routes/swagger.route')(app);
